4503d07b8afe9893ec067825cf730b9eff6b2f3c
rsvn <rizwanshaz2004@gmail.com>
2025-04-25 23:02:58 +0530

    code review(day-5)P-1

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -4,84 +4,82 @@
 #define local_persist static
 #define internal static
 typedef uint8_t uint8;
+typedef uint32_t uint32;
+
+
+struct win32_buffer 
+{
+ BITMAPINFO Info;
+ void *Memory;
+ int Width;
+ int Height;
+ int Pitch;
+ int BytesPerPixel=4;
+};
 
 global_variable bool running;
-global_variable BITMAPINFO BitmapInfo;
-global_variable void *BitmapMemory;
-global_variable int BitmapWidth;
-global_variable int BitmapHeight;
+global_variable  win32_buffer GlobalBackBuffer;
 
-internal void renderGradient(int xoffset,int yoffset){
+internal void renderGradient(win32_buffer Buffer,int xoffset,int yoffset){
 
-    int Width=BitmapWidth;
-    int Height=BitmapHeight;
-    int BytesPerPixel=4;
-    int Pitch=Width*BytesPerPixel;
-    uint8* Row =(uint8*)BitmapMemory;
-    for(int Y=0;Y<BitmapHeight;Y++)
-    {   uint8* Pixel=(uint8*)Row;
-        for(int X=0;X<BitmapWidth;X++){
+    uint8* Row =(uint8*)Buffer.Memory;
+    for(int Y=0;Y<Buffer.Height;Y++)
+    {   uint32* Pixel=(uint32*)Row;
+        for(int X=0;X<Buffer.Width;X++){
             //little endian notation BBGGRRXX Memory layout
             //Blue
-           *Pixel=(uint8)(X+xoffset);
-            ++Pixel;
+            uint8 Blue=(uint8)(X+xoffset);
+            uint8 Green=(uint8)(X+xoffset);
+           *Pixel++=((Green<<8)|Blue);
 
             //green
-           *Pixel=(uint8)(Y+yoffset);
-            ++Pixel;
-
-            //Red
-           *Pixel=0;
-            ++Pixel;
-
-            //XX 32-bit
-           *Pixel=0;
-            ++Pixel;
         }
-        Row+=Pitch;
+        Row+=Buffer.Pitch;
     }
 }
 
 internal void
-Win32ResizeDIBSection(int Width,int Height)   //Allocates (or reallocates) memory for the DIB section when the window is resized
+Win32ResizeDIBSection(win32_buffer *Buffer,int Width,int Height)   //Allocates (or reallocates) memory for the DIB section when the window is resized
 {
     //maybe dont free first,free after,then free first it that fails
 
-    if(BitmapMemory)
+    if(Buffer->Memory)
     {
-        VirtualFree(BitmapMemory,0,MEM_RELEASE);  //free old memory
+        VirtualFree(Buffer->Memory,0,MEM_RELEASE);  //free old memory
     }
 
-    BitmapWidth=Width;
-    BitmapHeight=Height;
+    Buffer->Width=Width;
+    Buffer->Height=Height;
 
-    BitmapInfo.bmiHeader.biSize=sizeof(BitmapInfo.bmiHeader) ;
-    BitmapInfo.bmiHeader.biWidth=BitmapWidth;
-    BitmapInfo.bmiHeader.biHeight=-BitmapHeight;
-    BitmapInfo.bmiHeader.biPlanes=1;
-    BitmapInfo.bmiHeader.biBitCount=32;
-    BitmapInfo.bmiHeader.biCompression=BI_RGB;
+    Buffer->Info.bmiHeader.biSize=sizeof(Buffer->Info.bmiHeader) ;
+    Buffer->Info.bmiHeader.biWidth=Buffer->Width;
+    Buffer->Info.bmiHeader.biHeight=-Buffer->Height;
+    Buffer->Info.bmiHeader.biPlanes=1;
+    Buffer->Info.bmiHeader.biBitCount=32;
+    Buffer->Info.bmiHeader.biCompression=BI_RGB;
 
-    int BytesPerPixel=4;
-    int BitmapMemorySize=(BitmapWidth *BitmapHeight)*BytesPerPixel;
-    BitmapMemory=VirtualAlloc(0,BitmapMemorySize,MEM_COMMIT,PAGE_READWRITE);
+    int BitmapMemorySize=(Buffer->Width *Buffer->Height)*Buffer->BytesPerPixel;
+    Buffer->Memory=VirtualAlloc(0,BitmapMemorySize,MEM_COMMIT,PAGE_READWRITE);
 
+
+
+     Buffer->Pitch=Width*Buffer->BytesPerPixel;
 }
 
 
 internal void
-Win32UpdateWindow(HDC DeviceContext,RECT *ClientRect,int X,int Y,int Width,int Height)   //Draw the current bitmap memory buffer onto the window.
+Win32CopyBufferToWindow(HDC DeviceContext,RECT ClientRect,win32_buffer Buffer,int X,int Y,int Width,int Height)   //Draw the current bitmap memory buffer onto the window.
 {
-    int WindowWidth = ClientRect->right - ClientRect->left;
-    int WindowHeight = ClientRect->bottom - ClientRect->top;                               // RECT structure, which describes the client area of the window 
+    int WindowWidth = ClientRect.right - ClientRect.left;
+    int WindowHeight = ClientRect.bottom - ClientRect.top;                               // RECT structure, which describes the client area of the window 
     StretchDIBits(                                                                       //Copies pixels from memory (BitmapMemory) to the window (DeviceContext).
         DeviceContext,
    //     X,Y,Width,Height,
    //     X,Y,Width,Height,
-        0,0,BitmapWidth,BitmapHeight,    //old DIB
+        0,0,Buffer.Width,Buffer.Height,    //old DIB
         0,0,WindowWidth,WindowHeight,      //new DIB
-        BitmapMemory,
-        &BitmapInfo,
+        Buffer.Memory,
+        &Buffer.Info,
         DIB_RGB_COLORS,
         SRCCOPY
     );
@@ -99,7 +97,7 @@ MainWindowCallback( HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
                 GetClientRect(Window,&ClientRect);
                 int Height= ClientRect.bottom - ClientRect.top;
                 int Width= ClientRect.right - ClientRect.left;
-                Win32ResizeDIBSection(Width,Height);
+                Win32ResizeDIBSection(&GlobalBackBuffer,Width,Height);
              OutputDebugStringA("WM_SIZE\n");
 
           } break;
@@ -124,7 +122,7 @@ MainWindowCallback( HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
                 RECT ClientRect; 
                 GetClientRect(Window,&ClientRect);
 
-                Win32UpdateWindow(DeviceContext,&ClientRect,X,Y,Width,Height);
+                Win32CopyBufferToWindow(DeviceContext,ClientRect,GlobalBackBuffer,X,Y,Width,Height);
                 EndPaint(Window,&Paint);
                }break;
           case WM_ACTIVATEAPP:
@@ -181,14 +179,14 @@ int CALLBACK WinMain( HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandL
                     DispatchMessage(&Message);                 
                 }
 
-                renderGradient(xoffset,yoffset);
+                renderGradient(GlobalBackBuffer,xoffset,yoffset);
                 
                 HDC DeviceContext=GetDC(Window);
                 RECT ClientRect; 
                 GetClientRect(Window,&ClientRect);
                 int WindowWidth = ClientRect.right - ClientRect.left;
                 int WindowHeight = ClientRect.bottom - ClientRect.top;                               // RECT structure, which describes the client area of the window 
-                Win32UpdateWindow(DeviceContext,&ClientRect,0,0,WindowWidth,WindowHeight);   //Draw the current bitmap memory buffer onto the window.
+                Win32CopyBufferToWindow(DeviceContext,ClientRect,GlobalBackBuffer,0,0,WindowWidth,WindowHeight);
                 ReleaseDC(Window,DeviceContext);
                 
                 ++xoffset;
