bb9002f880eb7ebadd9e1fd3af1f82171052a291
rsvn <rizwanshaz2004@gmail.com>
2025-07-16 15:32:13 +0530

    day-7 <dsound>

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <stdint.h>
 #include <xinput.h>
+#include <dsound.h>
 
 #define global_variable static
 #define local_persist static
@@ -59,14 +60,24 @@ global_variable x_input_set_state *XInputSetState_;
 #define XInputGetState XInputGetState_
 #define XInputSetState XInputSetState_
 
+
+// This macro defines a reusable DirectSound function signature.
+// It allows us to declare functions with this signature using any name.
+// The typedef below uses the macro to define a function *type* (not a function).
+// This creates an alias 'direct_sound_create' for the function signature.
+// We can now use it to declare function pointers (e.g., for GetProcAddress).
+#define DIRECT_SOUND_CREATE(name) HRESULT WINAPI name(LPCGUID pcGuidDevice, LPDIRECTSOUND *ppDS, LPUNKNOWN pUnkOuter)
+typedef DIRECT_SOUND_CREATE(direct_sound_create);
+
+
 internal void
 win32LoadXInput(void)
 {
 
-HMODULE WINAPI XInputLibrary = LoadLibrary("xinput_4.dll"); 
+HMODULE WINAPI XInputLibrary = LoadLibrary("xinput1_4.dll"); 
 if(!XInputLibrary)
   {
-    HMODULE WINAPI XInputLibrary = LoadLibrary("xinput_3.dll"); 
+    HMODULE WINAPI XInputLibrary = LoadLibrary("xinput1_3.dll"); 
   }
     if(XInputLibrary)
   { 
@@ -76,6 +87,96 @@ if(!XInputLibrary)
   }
 
 }
+
+
+internal void 
+Win32InitDSound(HWND Window,int32 SamplesPerSecond,int32 BufferSize)
+{
+  HMODULE DSoundLibrary = LoadLibraryA("dsound.dll");
+  if(DSoundLibrary)
+  {
+    // Steps to load a dynamic function using a function pointer:
+    // 1. Load the library (DLL/.so) at runtime
+    // 2. Get the function address using GetProcAddress() / dlsym()
+    // 3. Cast the address to the correct function pointer type
+    // 4. Assign it to a function pointer variable
+    // 5. Call the function through the pointer
+    // 6. Unload the library when done (FreeLibrary() / dlclose())
+
+    direct_sound_create *DirectSoundCreate =(direct_sound_create *)GetProcAddress(DSoundLibrary,"DirectSoundCreate");
+    //load the library
+    LPDIRECTSOUND DirectSound;
+    if(DirectSoundCreate && SUCCEEDED(DirectSoundCreate(0,&DirectSound,0)))
+    {           WAVEFORMATEX WaveFormat={};
+      WaveFormat.wFormatTag=WAVE_FORMAT_PCM;
+      WaveFormat.nChannels=2;
+      WaveFormat.nSamplesPerSec=SamplesPerSecond;
+      WaveFormat.wBitsPerSample=16;
+      WaveFormat.nBlockAlign=(WaveFormat.nChannels*WaveFormat.wBitsPerSample)/8;
+      WaveFormat.nAvgBytesPerSec=WaveFormat.nSamplesPerSec*WaveFormat.nBlockAlign;
+      WaveFormat.cbSize=0;
+
+
+
+      if(SUCCEEDED(DirectSound->SetCooperativeLevel(Window,DSSCL_PRIORITY)))
+      {
+        DSBUFFERDESC BufferDescription={};
+        BufferDescription.dwSize=sizeof(BufferDescription);
+        BufferDescription.dwFlags=DSBCAPS_PRIMARYBUFFER;
+
+
+        LPDIRECTSOUNDBUFFER PrimaryBuffer;
+        if(SUCCEEDED(DirectSound->CreateSoundBuffer(&BufferDescription,&PrimaryBuffer,0)))
+        {
+
+          if(SUCCEEDED(PrimaryBuffer->SetFormat(&WaveFormat)))
+          {
+            //finally set the format
+            OutputDebugStringA("Primary buffer format was set.\n");
+          }
+
+          else
+         {
+            //diagnostic errors:
+          }
+        }
+        else
+       {
+          //diagnostic errors:
+        }
+      }
+      else
+     {
+        //diagnostic errors:
+      }
+
+
+      DSBUFFERDESC BufferDescription={};
+      BufferDescription.dwFlags=0;
+      BufferDescription.dwBufferBytes=BufferSize;
+      BufferDescription.lpwfxFormat=&WaveFormat;
+      LPDIRECTSOUNDBUFFER SecondaryBuffer;
+      if(SUCCEEDED(DirectSound->CreateSoundBuffer(&BufferDescription,&SecondaryBuffer,0)))
+      {
+        //start playing
+       OutputDebugStringA("Secoundary buffer format was set.\n");
+      }
+    }
+
+    else
+   {
+      //diagnostic errors:
+    }
+
+  }
+
+  else
+  {
+    //direct sound fails to load and diagnostic errors:
+  }
+
+}
+
 
 
 internal win32_window_dimension 
@@ -300,9 +401,15 @@ int CALLBACK WinMain( HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandL
           Instance,
               0);
         if(Window)
-        { 
+        {
+
+      HDC DeviceContext=GetDC(Window);
                 int xoffset=0;
                 int yoffset=0;
+
+
+
+      Win32InitDSound(Window,48000,48000*sizeof(int16)*2);
            GlobalRunning=true;
             while(GlobalRunning)
             {
